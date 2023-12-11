acc64c446cb8d03795b4c2f57fb666c38ac0101c
Sanzia7 <meritee@gmx.de>
2023-12-11 11:00:08 +0100

    add js variant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 // import { createElement } from 'react';
 
-
 export const App = () => {
 
 	// return createElement(
 	// 	'div',
 	// 	{ className: 'App' },
-	// 	'header',
-	// 	{ className: 'App-header'},
-	// )
+	// 	createElement('header',
+   //    { className: 'App-header'}),
+
+   //    createElement('img',
+   //     { crs: 'logo'}
+   //     { className: 'App-logo'},
+   //      {alt: 'logo'}),
+
+   //    createElement('p', nul,
+   //    'Edit <code>src/App.js</code> and save to reload' ),
+   //      createElement('div', null, "hello world"),
+   //
+   //   createElement('a',
+   //  { className: 'App-link'},
+   //  { href: 'https://reactjs.org" },
+   //  { target: "_blank" },
+   //  { rel: "noopener noreferrer" },
+   //   "Learn React"),
+   //  createElement('p', {new Date().getFullYear()}, ),
+   // )
 
 	return (
 		<div className="App">
@@ -40,3 +56,12 @@ export const App = () => {
 
 
 //props (пропсы) — это входные данные React-компонентов, передаваемые от родительского компонента дочернему компоненту.
+
+//    createElement('i', null, name), это как раз вложеный тег
+// если div вложен в div то будет
+//    createElement(
+//       'div',
+//       { className: 'App' },
+//       createElement('div', null, "hello world"),
+//    );
+// }
